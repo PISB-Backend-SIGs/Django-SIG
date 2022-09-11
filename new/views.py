be5ae14091e153e0eb6b70894ee9c1323c7ff38b
crazygolden <amogh.thakurdesai@gmail.com>
2022-09-12 00:20:10 +0530

    Task 1

---
+++ new/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def printall(k):
+    out = []
+    if k>0:
+        for i in range(1,k+1):
+            out.append(i)
+        return out
+    else:
+        return ['Please enter positive number']        
+# Create your views here.
+def home(request):
+    n = int(request.GET.get('num','0'))
+    context = {'Num': n ,'func': printall(n)}
+    return render(request, "new/base.html",context)
