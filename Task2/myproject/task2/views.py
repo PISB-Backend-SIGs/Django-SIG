3f8b06178e545b7dfa2336360459205628aaee4e
prasad <hackerspider09@gmail.com>
2022-09-20 21:13:02 +0530

    Task 1.3

--- Task2/myproject/task2/views.py
+++ Task2/myproject/task2/views.py
@@ -9,7 +9,10 @@ from django.contrib.auth import authenticate,login,logout #for authentication sy
 def home(request):
     return render(request,'home.html')
 
+#for signup 
 def signup(request):
+    error=[]
+    ct=0
     if request.method=='POST':
         uname=request.POST['uname']
         fname=request.POST['fname']
@@ -19,33 +22,70 @@ def signup(request):
         gender=request.POST['gender']
         pass1=request.POST['pass1']
         pass2=request.POST['pass2']
+    
+    
 
-        #for authentication
-        if User.objects.filter(username=uname).exists() or User.objects.filter(email=email).exists():
-            error=[
-                "Username is already taken",
-                "Or",
-                "Email is already taken",]
+        #for authentication and errors
+        if len(phone)!=10:
+            error.append("Wrong phone number")
+            ct+=1
+        if  User.objects.filter(email=email).exists():
+            error.append("Email is already register")
+            ct+=1
+        if Data.objects.filter(phone_num=phone):
+            error.append("Phone is already register")
+            ct+=1
+        if User.objects.filter(username=uname).exists() :
+            error.append("Username is already register")
+            ct+=1
+        if (ct>=1):
             return render(request,'signup.html',{'errors':error})
+   
         else:
             if pass1==pass2:
-                #for store in data database
-                data=Data(user_name=uname,first_name=fname,last_name=lname,email_id=email,phone_num=phone,
-                gender_user=gender,
-                password_user=pass1)
-                data.save()
+                special =["@","#","_"]
+                a=0
+                d=0
+                s=0
+                c=0
+                if len(pass1)>=8:
+                    for i in pass1:
+                        if i.isupper():
+                            c+=1
+                        if i.isalpha():
+                            a+=1
+                        if i.isnumeric():
+                            d+=1
+                        if i in special:
+                            s+=1
+                    if a>=1 and d>=1 and s>=1 and c>=1:
+                        #for store in data database
+                        data=Data(user_name=uname,first_name=fname,last_name=lname,email_id=email,phone_num=phone,
+                        gender_user=gender,
+                        password_user=pass1)
+                        data.save()
 
-                #for store in userdatabase
-                user=User.objects.create_user(username=uname,first_name=fname,last_name=lname,email=email,password=pass1)
-                user.save()
+                        #for store in userdatabase
+                        user=User.objects.create_user(username=uname,first_name=fname,last_name=lname,email=email,password=pass1)
+                        user.save()
 
-                return render(request,'home.html',{'message':"Succesfuly Account Created"})
+                        return render(request,'home.html',{'message':"Succesfuly Account Created"})
+                    else :
+
+                        passmessage="Password should be atleast 8 digit \n It must contain atlest 1 uppercase character \n atleast 1 lowercase character \n atlest one special character"
+                        return render(request,'signup.html',{'passmessage':passmessage})
+                else :
+                    # print ("password should be greater than 7 character")
+                    return render(request,'signup.html',{'passmessage':"password should be greater than 7 character"})
+                
             else:
                 return render(request,'signup.html',{'passmessage':"Passwords are not same"})
                 
     else:
         return render(request,'signup.html')
 
+
+#for login 
 def login_user(request):
     if request.method=="POST":
         usern=request.POST['uname']
