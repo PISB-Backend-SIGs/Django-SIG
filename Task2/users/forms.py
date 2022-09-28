8ee9d4e61da865010a30618387da0c6ccf5e09d9
Aayush <aayushmohod4@gmail.com>
2022-09-28 22:51:56 +0530

    Task 2 modified

--- Task2/users/forms.py
+++ Task2/users/forms.py
@@ -7,10 +7,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    phone = forms.CharField(max_length=10)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'phone', 'password1', 'password2']
     
     def clean_email(self):
         email = self.cleaned_data.get('email')
@@ -19,3 +20,8 @@ class UserRegisterForm(UserCreationForm):
             raise forms.ValidationError("This email is already used")
 
         return email
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+
+        if phone.isnumeric() == False :
+            raise forms.ValidationError("Please enter a Valid Phone Number")
