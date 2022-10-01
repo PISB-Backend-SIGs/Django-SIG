fd0a7bb7e022d7e198b64ae53280461653e84e18
Aayush <aayushmohod4@gmail.com>
2022-10-01 23:11:31 +0530

    Task 2 modified

--- Task2/Core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    context={}
-    return render(request,'Core/home.html',context)
--- Task2/users/views.py
+++ Task2/users/views.py
@@ -1,19 +1,42 @@
+from email import message
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.contrib import messages
-from .forms import UserRegisterForm
+import re
 
 def register(request):
+    message = []
     if request.method == 'POST' :
-        form = UserRegisterForm(request.POST)
-        if form.is_valid() :
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('register')
-    else :    
-        form = UserRegisterForm()
+        newusername = request.POST['newusername']
+        mobile = request.POST['Mobile']
+        emai_l = request.POST['Email']
+        pas = request.POST['Password']
+        confirmPassword = request.POST['ConfirmPassword']
+        if User.objects.filter(username = newusername).exists() :
+            message.append("Username already  Exists")
+        elif mobile.isnumeric() == False or len(mobile) != 10 :
+            message.append("Enter a valid Mobile number")
+        elif  User.objects.filter(email = emai_l).exists():
+            message.append("Email is already register")
+        elif pas != confirmPassword :
+            message.append("Confirmed Password did not match the entered Password")
+        if (len(pas) < 8):
+             message.append("Password should contain atleast 8 characters")
+        elif not re.search("[a-z]", pas):
+            message.append("Password should contain atleast one Lowercase letter")
+        elif not re.search("[A-Z]", pas):
+            message.append("Password should contain atleast one Uppercase letter")
+        elif not re.search("[0-9]", pas):
+            message.append("Password should contain atleast one Number")
+        elif not re.search("[_@#%$]", pas):
+            message.append("Password should contain atleast one Special character")
+        else :
+            message.append("New User created Succesfully")
+            User.objects.create_user(username=newusername,password=pas,email=emai_l)
     context = {
-        'form':form
-    }
+        'messages' : message
+    }    
     return render(request, 'users/register.html', context)
 
+def content(request):
+    return render(request, 'users/content.html')
